74c4b57f259e94431b38df011f09a4ce76948fa5
henriquesantosdev <henriquesantos2277@gmail.com>
2024-08-06 15:19:09 -0300

    Finish base routting

--- nodejs-backend/src/server.ts
+++ nodejs-backend/src/server.ts
@@ -1,11 +1,25 @@
 import fastify from "fastify";
+import { prisma } from "./lib/prisma";
 
 const app = fastify()
 
-app.get('/teste', () => {
-    return 'Hello, world!'
+app.get('/cadastrar', async () => {
+    await prisma.trip.create({
+        data: {
+            destination: 'Paraiba',
+            starts_at: new Date(),
+            ends_at: new Date()
+        }
+    })
+
+    return "Registro cadastrado com sucesso!"
+})
+
+app.get('/listar', async () => {
+    const trips = await prisma.trip.findMany()
+    return trips
 })
 
 app.listen({port: 3333}).then((address) => {
-    console.log(`🚀 A giripoca está piando em: \x1b[32m${address}`)
+    console.log(`🚀 Server is running on: \x1b[32m${address}`)
 })
