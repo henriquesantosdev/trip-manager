ea12bc8f0a09a3c1402464224615cb2f988f42e1
henriquesantosdev <henriquesantos2277@gmail.com>
2024-08-06 16:20:49 -0300

    Add lib to validation

--- nodejs-backend/src/server.ts
+++ nodejs-backend/src/server.ts
@@ -1,24 +1,13 @@
 import fastify from "fastify";
-import { prisma } from "./lib/prisma";
+import { createTrip } from "./routes/create-trip";
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 
 const app = fastify()
 
-app.get('/cadastrar', async () => {
-    await prisma.trip.create({
-        data: {
-            destination: 'Paraiba',
-            starts_at: new Date(),
-            ends_at: new Date()
-        }
-    })
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
 
-    return "Registro cadastrado com sucesso!"
-})
-
-app.get('/listar', async () => {
-    const trips = await prisma.trip.findMany()
-    return trips
-})
+app.register(createTrip)
 
 app.listen({port: 3333}).then((address) => {
     console.log(`🚀 Server is running on: \x1b[32m${address}`)
