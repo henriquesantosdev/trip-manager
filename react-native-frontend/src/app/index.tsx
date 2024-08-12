4a0935096bf7164a312b1614acc8656772c1c71b
henriquesantosdev <henriquesantos2277@gmail.com>
2024-08-11 23:41:44 -0300

    configuring tailwind on react native project

---
+++ react-native-frontend/src/app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native"
+
+export default function index  ()
+{
+    return (
+        <View>
+            <Text>
+                Hello, world!
+            </Text>
+        </View>
+    )
+}
